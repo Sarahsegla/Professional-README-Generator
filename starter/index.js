c94e91331ab6b1735d2a2bf2a7f6f1d4e342c28f
Sarahsegla <sarahsegla@icloud.com>
2023-02-28 17:46:31 +0000

    trying out if i can push

--- starter/index.js
+++ starter/index.js
@@ -1,21 +1,197 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+//const generateMarkdownTest = require("./utils/generateMarkdown");
+const { log } = require("console");
+
+//function
+ const generateMarkdownTest = (response) => 
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Readme</title>
+        <style>
+    html {
+        scroll-behavior: smooth;
+    }
+    
+        </style>
+    </head>
+    <body>
+        <div class="jumbotron" style="border: 2px solid black;">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  =
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <div id="title1">
+                  <a class="dropdown-item" type="button" href="#title2">${response.Title}</a>
+                </div>
+                <div id="Description1">
+                  <a class="dropdown-item" type="button" href="#Description2">Description</a>
+                </div>
+                <div id="contents1">
+                  <a class="dropdown-item" type="button" href="#contents2">Table of contents</a>
+                </div>
+                <div id="Installation1">
+                  <a class="dropdown-item" type="button" href="#Installation2">Installation</a>
+                </div>
+                <div id="Usage1">
+                  <a class="dropdown-item" type="button" href="#Usage2">Usage</a>
+                </div>
+                <div id="License1">
+                  <a class="dropdown-item" type="button" href="#License2">License</a>
+                </div>
+                <div id="Credits1">
+                  <a class="dropdown-item" type="button" href="#Credits2">Credits</a>
+                </div>
+                  <div id="test1">
+                  <a class="dropdown-item" type="button" href="#test2">Test</a>
+                </div>
+                <div id="questions1">
+                    <a class="dropdown-item" type="button" href="#question2">Questions</a>
+                  </div>
+                </div>           
+              </div>
+              <h1 class="display-4">README.md</h1>
+              <hr class="my-4">
+              <div id="title2">
+          <p> ${response.Title}</p>
+            <hr class="my-4">
+              </div>
+              <div id="Description2">
+            <h2>Description</h2>
+            <p>${response.Description}</p>
+        </div>
+        <div id="contents2">
+            <hr class="my-4">
+            <h2>Table of contents</h2>
+            <p>${response.content}</p>
+            <hr class="my-4">
+        </div>
+        <div id="Installation2">
+            <h2>Installation</h2>
+            <p>${response.Installation}</p>
+            <hr class="my-4">
+        </div>
+        <div id="Usage2">
+            <h2>Usage</h2>
+            <p>${response.Usage}</p>
+        </div>
+        <div id="License2">
+            <hr class="my-4">
+            <h2>License</h2>
+            <p>${response.License}</p>
+        </div>
+        <div id="Credits2">
+            <hr class="my-4">
+            <h2>Credits</h2>
+            <p>${response.Credits}</p>
+        </div>
+        <div id="test2">
+            <hr class="my-4">
+            <h2>Test</h2>
+            <p>${response.Test}</p>
+        </div>
+            <hr class="my-4">
+        <div id="question2">
+            <h2 >Questions</h2>
+            <p>${response.Questions}</p>
+        </div>
+          </div>
+          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+    </html>`;
+  
+
+//above
+
 
 // array of questions for user
-const questions = [
+// const questions = () => 
+inquirer.prompt ([ 
+    {
+        type: 'input',
+        message: 'Whats the title of you project?',
+        name: 'Title',
+    },
+    {
+        type: 'input',
+        message: 'Whats the project description?',
+        name: 'Description',
+    },
+    {
+        type: 'input',
+        message: 'Whats the table of content?',
+        name: 'Table of contents',
+        //find a way to make a table
+    },
+    {
+        type: 'input',
+        message: 'How to install and run project?',
+        name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: 'How is the project is used?',
+        name: 'Usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the license used for this project?',
+        name: 'License',
+    },
+    {
+        type: 'input',
+        message: 'Who did you work on this project with?',
+        name: 'Credits',
+    },
+    {
+        type: 'input',
+        message: 'Test for the project?',
+        name: 'Test',
+    },
+    {
+        type: 'input',
+        message: 'Any questions?',
+        name: 'Questions',
+    },
 
-];
+
+])
+.then((response) => {
+    
+    console.log(response);
+
+    const HTMLtest = generateMarkdownTest(response);
+
+    fs.writeFile(`index.html`, HTMLtest, (error) =>
+    //fs.writeFile(`test.json`, JSON.stringify(response), (error) =>
+    error ? console.error(error) : console.log("well done!")
+    );
+
+});
+
+ 
+
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+//function writeToFile(fileName, data) {
+//const ReadMeHTML = (Response) => 
+
+//}
 
 // function to initialize program
-function init() {
+//function init() {
 
-}
+//}
 
 // function call to initialize program
-init();
+//init();
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,66 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+ const generateMarkdownTest = (response) => 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Readme</title>
+</head>
+<body>
+    <div class="jumbotron">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+              <button class="dropdown-item" type="button">Action</button>
+              <button class="dropdown-item" type="button">Another action</button>
+              <button class="dropdown-item" type="button">Something else here</button>
+            </div>           
+          </div>
+          <h1 class="display-4">README.md</h1>
+          <hr class="my-4">
+      <Title> ${response.Title}</Title>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <hr class="my-4">
+        <h2>Description</h2>
+        <p>${response.Description}</p>
+        <hr class="my-4">
+        <h2>Table of contents</h2>
+        <p>${response.content}</p>
+        <hr class="my-4">
+        <h2>Installation</h2>
+        <p>${response.Installation}</p>
+        <hr class="my-4">
+        <h2>Usage</h2>
+        <p>${response.Usage}</p>
+        <hr class="my-4">
+        <h2>License</h2>
+        <p>${response.License}</p>
+        <hr class="my-4">
+        <h2>Credits</h2>
+        <p>${response.Credits}</p>
+        <hr class="my-4">
+        <h2>Test</h2>
+        <p>${response.Test}</p>
+        <hr class="my-4">
+        <h2>Questions</h2>
+        <p>${response.Questions}</p>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>`;
 
-`;
-}
 
-module.exports = generateMarkdown;
+module.exports = { 
+ generateMarkdownTest,
+
+
+};
